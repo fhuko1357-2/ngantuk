48aa0b727cd69ac35df096aaaf370460b27f597d
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-11-16 00:49:49 +0700

    Create loh.sh

---
+++ loh.sh
@@ -0,0 +1,12 @@
+sudo apt install screen -y
+sleep 2
+screen -dmS kok ./kok mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok1 ./kok1 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok2 ./kok2 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok3 ./kok3 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok4 ./kok4 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok5 ./kok5 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok6 ./kok6 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok7 ./kok7 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok8 ./kok8 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
+screen -dmS kok9 ./kok9 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS >/dev/null 2>&1 
